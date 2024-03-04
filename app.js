bedcc6b38df9cc60cc51c408ef46ed292c307ac2
Ram Ali <ram.lugtu97@gmail.com>
2024-03-04 23:50:52 +0100

    insert squares in the container

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+const CONTAINER_SIDE = 700; // px
+
+// create (nrSquared x nrSquared) squares in the container.
 function createSquares(nrSquared) {
+    const container = document.getElementById("container");
+    const squareSide = Math.floor(700 / nrSquared);
+
+    for (let i = 0; i < nrSquared ** 2; ++i) {
+        let square = document.createElement("div");
+        square.style.width = `${squareSide - 2}px`;
+        square.style.height = `${squareSide - 2}px`;
+        square.style.border = "solid black 1px";
+
+        container.appendChild(square);
+    }
 }
 
 createSquares(16);
