2752a038b1c46b65baa49a760e4c93ccb746bc6e
Ram Ali <ram.lugtu97@gmail.com>
2024-03-05 22:05:34 +0100

    remove spaces between squares with proper css

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ function createSquares(nrSquared) {
 
     for (let i = 0; i < nrSquared ** 2; ++i) {
         let square = document.createElement("div");
-        square.style.width = `${squareSide - 2}px`;
-        square.style.height = `${squareSide - 2}px`;
-        square.style.border = "solid black 1px";
+        square.style.border = "solid lightgray 1px";
 
         container.appendChild(square);
     }
