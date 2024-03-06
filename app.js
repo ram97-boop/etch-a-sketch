0e31903762ea91ad3136d40e304d7f5187e1f2cc
Ram Ali <ram.lugtu97@gmail.com>
2024-03-06 20:39:24 +0100

    refactor, convert magic numbers to global variables

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const CONTAINER_SIDE = 700; // px
+const CONTAINER_SIDE_LENGTH = 700; // px
+const CONTAINER = document.getElementById("container");
+const SQUARES_LIMIT = 100;
 
 // create (nrSquared x nrSquared) squares in the container.
 function createSquares(nrSquared) {
-    const container = document.getElementById("container");
-    const squareWidth = Math.floor(700 / nrSquared);
+    const squareWidth = Math.floor(CONTAINER_SIDE_LENGTH / nrSquared);
 
     for (let i = 0; i < nrSquared ** 2; ++i) {
         let square = document.createElement("div");
         square.style.minWidth = `${squareWidth}px`;
 
-        container.appendChild(square);
+        CONTAINER.appendChild(square);
     }
 }
 
-const container = document.getElementById("container");
-container.addEventListener("mouseover", (event) => {
+CONTAINER.addEventListener("mouseover", (event) => {
     const target = event.target;
 
     if (target.id !== "container") // if target is one of the squares
@@ -24,20 +24,20 @@ container.addEventListener("mouseover", (event) => {
 const button = document.getElementById("buttonNew");
 button.addEventListener("click", () => {
     let squaresPerSide = parseInt(
-        prompt("Number of squares per side (max. 100): ")
+        prompt(`Number of squares per side (max. ${SQUARES_LIMIT}): `)
     );
 
-    if (squaresPerSide > 100) {
-        squaresPerSide = 100;
+    if (squaresPerSide > SQUARES_LIMIT) {
+        squaresPerSide = SQUARES_LIMIT;
     }
 
-    clearContainer(document.getElementById("container"));
+    clearContainer();
     createSquares(squaresPerSide);
 })
 
-function clearContainer(container) {
-    while (container.lastElementChild) {
-        container.removeChild(container.lastElementChild);
+function clearContainer() {
+    while (CONTAINER.lastElementChild) {
+        CONTAINER.removeChild(CONTAINER.lastElementChild);
     }
 }
 
